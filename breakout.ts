f5a77f54b3741347e86b8b7e05d82acd8f7cf1bb
Sampow360 <134953678+Sampow360@users.noreply.github.com>
2023-07-28 16:57:27 -0500

    1 binaries uploaded

--- breakout.ts
+++ breakout.ts
@@ -1,6 +1,25 @@
-// Import stylesheets
-import './style.css';
+// This object contains all the Phaser configurations to load our game
+const config = {
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 800,
+  heigth: 640,
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  scene: {
+    preload,
+    create,
+    update,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: false
+    },
+  }
+};
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+// Create the game instance
+const game = new Phaser.Game(config);
